acfa7531b607dcacfb216fd4a5fb928b26cb3458
Ivan Diaz <feleir@gmail.com>
2018-03-24 11:27:56 +0100

    Added animation on card flip.

--- components/Quiz/Question.js
+++ components/Quiz/Question.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Button, Text, TouchableOpacity } from 'react-native'
+import { View, Button, Text, TouchableOpacity, Animated } from 'react-native'
 
 import { red, green } from '../../utils/colors'
 import styles from './styles'
@@ -7,18 +7,24 @@ import styles from './styles'
 export default class Question extends Component {
     state = {
         showQuestion: true,
+        bounceValue: new Animated.Value(1)
     }
 
     componentWillReceiveProps() {
         this.setState(() => ({ showQuestion: true }))
     }
     flipCard = () => {
+        const { bounceValue } = this.state
+        Animated.sequence([
+            Animated.timing(bounceValue, { duration: 200, toValue: 1.5}),
+            Animated.spring(bounceValue, { toValue: 1, friction: 4})
+        ]).start()
         this.setState(state => ({ showQuestion: !state.showQuestion }))
     }
 
     render() {
         const { question, handleAnswer, index, length } = this.props
-        const { showQuestion } = this.state
+        const { showQuestion, bounceValue } = this.state
 
         const cardText = showQuestion ? question.question : question.answer
         const buttonText = showQuestion ? 'Show answer' : 'Show question'
@@ -26,12 +32,14 @@ export default class Question extends Component {
         return (
             <View style={styles.card}>
                 <Text style={styles.text}>{`${index + 1} / ${length}`}</Text>
-                <TouchableOpacity style={styles.cardTextView} onPress={this.flipCard}>
-                    <Text style={[styles.text, styles.header]}>{cardText}</Text>
-                    <Text style={[styles.text, styles.flipButton, styles.separatedItem]}>
-                    {buttonText}
-                    </Text>
-                </TouchableOpacity>
+                <Animated.View  style={[styles.cardTextView, { transform: [{scale: bounceValue}] }]}>
+                    <TouchableOpacity onPress={this.flipCard}>
+                        <Text style={[styles.text, styles.header]}>{cardText}</Text>
+                        <Text style={[styles.text, styles.flipButton, styles.separatedItem]}>
+                            {buttonText}
+                        </Text>
+                    </TouchableOpacity>
+                </Animated.View>
                 <Button
                     title="Correct"
                     color={green}
